dfa924784bdad941879296b98997c2cba56665ea
Mingzhe Zou <zoumingzhe@qq.com>
2023-08-23 16:19:59 +0800

    add checker class

--- xarg/parser.py
+++ xarg/parser.py
@@ -6,10 +6,63 @@ from argparse import _ArgumentGroup
 from argparse import _SubParsersAction
 from typing import Optional
 
-from .checker import check_name_opt
-from .checker import check_name_pos
-from .checker import check_nargs_opt
 from .util import URL_PROG
+
+
+class checker():
+
+    prefix_chars = "-"
+
+    @classmethod
+    def check_name_pos(cls, fn):
+        '''
+        check positional argument name
+        '''
+
+        def inner(self, name: str, **kwargs):
+            assert isinstance(name, str) and name[0] not in cls.prefix_chars,\
+                f"{name} is not a positional argument name"
+            return fn(self, name, **kwargs)
+
+        return inner
+
+    @classmethod
+    def check_name_opt(cls, fn):
+        '''
+        check optional argument name
+        '''
+
+        def inner(self, *name: str, **kwargs):
+            # 1. check short form optional argument ("-x")
+            # 2. check long form optional argument ("--xx")
+            # 3. only short form or long form or short form + long form
+            # 模棱两可的数据（-1可以是一个负数的位置参数）
+            for n in name:
+                assert isinstance(n, str) and n[0] in cls.prefix_chars,\
+                    f"{n} is not an optional argument name"
+            return fn(self, *name, **kwargs)
+
+        return inner
+
+    @classmethod
+    def check_nargs_opt(cls, fn):
+        '''
+        nargs hook function:
+            nargs < -1: using "?", 0 or 1 argument, default value
+            nargs = -1: using "+", arguments list, at least 1
+            nargs = 0: using "*", arguments list, allow to be empty
+            nargs = 1: redirect to "?", 0 or 1 argument
+            nargs > 1: N arguments list
+        '''
+
+        def inner(self, *args, **kwargs):
+            _nargs = kwargs.get("nargs", -2)
+            if isinstance(_nargs, int) and _nargs < 2:
+                _nargs = {1: "?", 0: "*", -1: "+"}.get(_nargs, "?")
+            kwargs.update({"nargs": _nargs})
+            return fn(self, *args, **kwargs)
+
+        return inner
 
 
 class argp(ArgumentParser):
@@ -41,7 +94,7 @@ class argp(ArgumentParser):
                 return group
         return self.add_argument_group(title, description, **kwargs)
 
-    @check_name_opt
+    @checker.check_name_opt
     def filter_optional_name(self, *name: str) -> set:
         '''
         Filter defined optional argument name.
@@ -51,7 +104,7 @@ class argp(ArgumentParser):
             option_strings.update(action.option_strings)
         return set(name) - option_strings
 
-    @check_name_pos
+    @checker.check_name_pos
     def add_pos(self, name: str, **kwargs) -> None:
         '''
         Add positional argument.
@@ -60,15 +113,15 @@ class argp(ArgumentParser):
             "dest supplied twice for positional argument"
         self.add_argument(name, **kwargs)
 
-    @check_name_opt
-    @check_nargs_opt
+    @checker.check_name_opt
+    @checker.check_nargs_opt
     def add_opt(self, *name: str, **kwargs) -> None:
         '''
         Add optional argument.
         '''
         self.add_argument(*name, **kwargs)
 
-    @check_name_opt
+    @checker.check_name_opt
     def add_opt_on(self, *name: str, **kwargs) -> None:
         '''
         Add boolean optional argument, default value is False.
@@ -78,7 +131,7 @@ class argp(ArgumentParser):
             assert key not in kwargs, f"'{key}' is an invalid argument"
         self.add_argument(*name, **kwargs)
 
-    @check_name_opt
+    @checker.check_name_opt
     def add_opt_off(self, *name: str, **kwargs) -> None:
         '''
         Add boolean optional argument, default value is True.
