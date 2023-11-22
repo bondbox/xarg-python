2bd901e735d62d13a20d897aa5a90d849016f32b
Mingzhe Zou <zoumingzhe@qq.com>
2023-11-22 16:24:44 +0800

    version: 0.11.beta.1

--- xarg/util.py
+++ xarg/util.py
@@ -4,6 +4,6 @@
 __package_name__ = "xarg-python"
 __prog_name__ = "xarg"
 __prog_complete__ = "xargcomplete"
-__version__ = "0.11.alpha.2"
+__version__ = "0.11.beta.1"
 
 URL_PROG = "https://github.com/bondbox/xarg-python"
