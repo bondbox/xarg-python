e748b88a5b966e3246441820b1a8d288e7009766
Mingzhe Zou <zoumingzhe@qq.com>
2024-02-27 17:17:31 +0800

    setup: attribute: fix __url_bugs__ error

--- xarg/attribute.py
+++ xarg/attribute.py
@@ -5,7 +5,7 @@ from urllib.parse import urljoin
 __name__ = "xarg-python"
 __version__ = "1.3"
 __description__ = "Simple command-line tool based on argparse."
-__url_home__ = "https://github.com/bondbox/xarg-python"
+__url_home__ = "https://github.com/bondbox/xarg-python/"
 __url_code__ = __url_home__
 __url_docs__ = __url_home__
 __url_bugs__ = urljoin(__url_home__, "issues")
